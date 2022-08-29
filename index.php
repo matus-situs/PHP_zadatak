8ee1df341f11b477419c70bb36c45afd77d2a36d
matej <nseal811@gmail.com>
2022-08-29 14:14:58 +0200

    rute na index i ovisno što je u url-u upisano poziva potrebnu funkciju

--- index.php
+++ index.php
@@ -1,17 +1,29 @@
 <?php
 include_once("User.php");
 
+$url = 'http://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+
 $user = new User();
+
+if (strpos($url,'/users/') !== false) {
+    if(isset($_GET["user_id"])) {
+        var_dump($user->fetchByID($_GET["user_id"]));
+    } else {
+        var_dump($user->all());
+    }
+}
+
+
 /*$user->Username = "orginal";
 $user->Password = "orginal";
 $user->save();
 $user->Username = "promjenjen";
-$user->Password = "promjenjen";*/
+$user->Password = "promjenjen";
 $user->addTimestamps();
 echo "<br>";
 var_dump($user->fetchByID(12));
 echo "<br>";
-//var_dump($user->fetchByAttribute("Username", "pero"));
+var_dump($user->fetchByAttribute("Username", "pero"));
 echo "<br>";
-//var_dump($user->all());
-echo "<br>";
+var_dump($user->all());
+echo "<br>";*/
